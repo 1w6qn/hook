f53b4a6219c88ffe6813627e9cb0778a9d81a557
1w6qn <2050903851@qq.com>
2024-02-01 15:13:20 +0800

    test

--- index.ts
+++ index.ts
@@ -14,41 +14,38 @@ function fuckACESDK() {
     };
   });
 }
-function changeUrl(serverUrl: String) {
+function NetworkHook(serverUrl: String) {
+  log("Network hook started")
   Java.perform(function () {
-    var sdk = Java.use("com.hypergryph.platform.hgsdk.contants.SDKConst$UrlInfo")
+    const sdk = Java.use("com.hypergryph.platform.hgsdk.contants.SDKConst$UrlInfo")
     sdk.getRemoteUrl.implementation = function () {
       log("[Java Layer]Hooked HGSDK")
       return `http://${serverUrl}`
     }
-    var URL = Java.use("java.net.URL");
-    URL.$init.overload('java.lang.String').implementation = function (urlStr) {
+    const URL = Java.use("java.net.URL");
+    URL.$init.overload('java.lang.String').implementation = function (urlStr:string) {
       log("[Java Layer]url:" + urlStr)
+      if(urlStr.match("anticheat")){
+        urlStr="http://127.0.0.1"
+      }
       return this.$init(urlStr);
     };
   });
+  Il2Cpp.perform(function(){
+    const Networker = Il2Cpp.domain.assembly("Assembly-CSharp").image.class("Torappu.Network.Networker");
+    const GetOverrideRouterUrl = Networker.method<Il2Cpp.String>("get_overrideRouterUrl");
+    GetOverrideRouterUrl.implementation = function () :Il2Cpp.String{
+      return Il2Cpp.string(`http://${serverUrl}/config/prod/official/network_config`);
+    }
+    const SendGet = Networker.method<Il2Cpp.Object>("SendGet");
+    //@ts-ignore
+    SendGet.implementation = function (a: Il2Cpp.String, b: Il2Cpp.String) {
+      log("[Il2Cpp Layer]Hooked SendGet Url:" + a + " Param:" + b)
+      return this.method("SendGet").invoke(a, b);
+    }
+  })
 }
-function confighook() {
-  Il2Cpp.perform(() => {
-    const Assembly_CSharp = Il2Cpp.domain.assembly("Assembly-CSharp")
-    const c = Assembly_CSharp.image.class("Torappu.SDK.U8ExternalTools.InitExtConfig.NetworkOptions")
-      .method(".ctor")
-    // detailed trace, it traces method calls and returns and it reports every parameter
-    c.implementation = function () {
-      var conf = this.method<Il2Cpp.Object>(".ctor").invoke();
-      //@ts-ignore
-      //conf.field("sdkServerUrl").value = "http://192.168.0.7:8000/";//@ts-ignore
-      //conf.field("gameServerUrl").value = "http://192.168.0.7:8000/";//@ts-ignore
-      //conf.field("u8ServerUrl").value = "http://192.168.0.7:8000/";
-      log(conf.toString())
-      log("hooked config")
-      return conf
-    };
-  });
-  /*
-  
-  */
-}
+
 function dump() {
   Il2Cpp.perform(() => {
     Il2Cpp.dump("dump.cs")
@@ -57,34 +54,34 @@ function dump() {
 }
 function md5rsahook() {
   Il2Cpp.perform(() => {
-    const VerifySignMD5RSA = Il2Cpp.domain.assembly("Assembly-CSharp").image.
-      class("Torappu.CryptUtils").method<boolean>("VerifySignMD5RSA");
+    const CryptUtils=Il2Cpp.domain.assembly("Assembly-CSharp").image.class("Torappu.CryptUtils");
+    const VerifySignMD5RSA = CryptUtils.method<boolean>("VerifySignMD5RSA");
+    const VerifySignMD5RSA2 = VerifySignMD5RSA.overload("System.Byte[]","System.Byte[]","System.String");
     //@ts-ignore
     VerifySignMD5RSA.implementation = function (a: Il2Cpp.String, b: Il2Cpp.String, c: Il2Cpp.String): boolean {
       log("[Il2Cpp Layer]Hooked VerifySignMD5RSA")
-      return true;
+      if(a.toString().match("sign")){
+        return true;
+      }
+      return CryptUtils.method<boolean>("VerifySignMD5RSA").invoke(a,b,c);
     };
-
+    //@ts-ignore
+    VerifySignMD5RSA2.implementation = function (a,b,c): boolean {
+      log("[Il2Cpp Layer]Hooked VerifySignMD5RSA2")
+      if(a.toString().match("sign")){
+        return true;
+      }
+      return CryptUtils.method<boolean>("VerifySignMD5RSA")
+        .overload("System.Byte[]","System.Byte[]","System.String").invoke(a,b,c);
+    };
   });
 }
-function test() {
+function Trace() {
   Il2Cpp.perform(() => {
-    log("[Il2Cpp Layer]Trace started")
-    const Networker = Il2Cpp.domain.assembly("Assembly-CSharp").image
-      .class("Torappu.Network.Networker");
-    const SendGet = Networker.method<Il2Cpp.Object>("SendGet");
-
-    //@ts-ignore
-    SendGet.implementation = function (a: Il2Cpp.String, b: Il2Cpp.String) {
-      log("[Il2Cpp Layer]Hooked SendGet")
-      log("[Il2Cpp Layer]Hooked SendGet Url:" + a + " Param:" + b)
-      return this.method("SendGet").invoke(a, b);
-    }
-    const GetOverrideRouterUrl = Networker.method<Il2Cpp.String>("get_overrideRouterUrl");
-    GetOverrideRouterUrl.implementation = function () :Il2Cpp.String{
-      return Il2Cpp.string("http://192.168.0.6:8000/config/prod/official/network_config");
-    }
-    Il2Cpp.trace().classes(Networker).and().attach();
+    const CryptUtils=Il2Cpp.domain.assembly("Assembly-CSharp").image.class("Torappu.CryptUtils");
+    Il2Cpp.trace().classes(CryptUtils).and().attach()
+    
+    
 
   });
 }
@@ -92,7 +89,9 @@ function test() {
 rpc.exports = {
   init(stage, parameters) {
     log("inited")
-    test()
+    md5rsahook()
+    Trace()
+    NetworkHook("192.168.0.6:8000")
     
   },
   dispose() {
